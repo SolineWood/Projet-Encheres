4c76e69043a4fb07011eb082cb1b9172cbbec352
Soline Foret <soline.foret2022@campus-eni.fr>
2022-03-28 15:51:22 +0200

    initialisation du projet

---
+++ src/main/java/fr/eni/enchere/bo/ArticleVendu.java
@@ -0,0 +1,16 @@
+package fr.eni.enchere.bo;
+
+import java.time.LocalDate;
+
+public class ArticleVendu {
+	
+	private String noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private Integer miseAPrix;
+	private Integer prixVente;
+	private String etatVente;
+
+}
