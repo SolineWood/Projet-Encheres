859f650e93a4044ee9eb7432fdec74ceb46ffad7
Soline Foret <soline.foret2022@campus-eni.fr>
2022-03-29 12:13:20 +0200

    Ajout connexion + début ArticleVendu dal

---
+++ src/main/java/fr/eni/enchere/dal/ArticleVenduDAO.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.eni.enchere.dal;
+
+import java.util.List;
+
+import fr.eni.enchere.bo.ArticleVendu;
+
+/**
+ * Classe en charge de
+ * @author sforet2022
+ * @date 29 mars 2022
+ * @version ENI-Encheres- V0.1
+ * @since  29 mars 2022 - 11:53:23
+ *
+ */
+public interface ArticleVenduDAO {
+	
+	public void insert (ArticleVendu articleVendu);
+	public List<ArticleVendu> selectAll();
+
+}
---
+++ src/main/java/fr/eni/enchere/dal/ArticleVenduDAOMock.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fr.eni.enchere.dal;
+
+/**
+ * Classe en charge de
+ * @author sforet2022
+ * @date 29 mars 2022
+ * @version ENI-Encheres- V0.1
+ * @since  29 mars 2022 - 12:01:45
+ *
+ */
+public class ArticleVenduDAOMock  {
+
+}
