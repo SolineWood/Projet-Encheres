e6a0f94c985214ea75915501833fab65e5980514
quentin deboudt <deboudtquentin@gmail.com>
2022-03-29 16:36:11 +0200

    dal -> Retrait -> Dal de Retrait fini..ou pas

--- src/main/java/fr/eni/enchere/dal/RetraitDAOImp.java
+++ src/main/java/fr/eni/enchere/dal/RetraitDAOImp.java
@@ -22,35 +22,18 @@ import fr.eni.enchere.dal.util.ConnectionProvider;
  * @author qdeboudt2022
  * 29 mars 2022
  */
-public class RetraitDAOImp {
+public class RetraitDAOImp implements RetraitDAO {
 	private final String SELECT = "SELECT no_article, rue, code_postal, ville FROM RETRAITS";
 	private final String INSERT = "INSERT INTO RETRAITS (no_article,rue,code_postal,ville) VALUES (?,?,?,?)";
-	private final String recupe = "join ARTICLES_VENDUS on RETRAITS.no_article = ARTICLES_VENDUS.no_article";
-	
-	public void selectNo_article() throws Exception {
-		try(Connection con = ConnectionProvider.getConnection()) {
-			List<ArticleVendu> result= new ArrayList<ArticleVendu>();
-			PreparedStatement stmt = con.prepareStatement(recupe);
-			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				ArticleVendu articleVendu = new ArticleVendu(rs.getInt("no_article"), null, null, null, null, null, null, null, null, null);
-				result.add(articleVendu);
-			}
-			
-			
-		}catch (SQLException e) {
-			throw new Exception("Probleme de Insert -> (RetraitDAOImpl)");
-		}
-	}
-	
-	
+	private final String recupe = "SELECT no_article FROM ARTICLES_VENDUS"
+			+ " join ARTICLES_VENDUS on RETRAITS.no_article = ARTICLES_VENDUS.no_article";
 	
 	
 	public void insert(Retrait retrait) throws Exception {
+		ArticleVendu articleVendu = new ArticleVendu();
 		try(Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(INSERT);
-			PreparedStatement stmt2 = con.prepareStatement(recupe);
-			stmt2.setString(1, rs.getInt("no_article")  );
+			stmt.setInt(1, articleVendu.getNoArticle());
 			stmt.setString(2, retrait.getRue());
 			stmt.setString(3, retrait.getCode_postal());
 			stmt.setString(4, retrait.getVille());
@@ -63,15 +46,13 @@ public class RetraitDAOImp {
 	}
 	
 	
-	
-	
 	public List<Retrait> selectAll() throws Exception {
 		List<Retrait> result= new ArrayList<Retrait>();
 		try(Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(SELECT);
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
-				Retrait retrait = new Retrait(rs.getInt("no_article"),rs.getString("rue") ,rs.getString("code_postal"),rs.getString("ville") );
+				Retrait retrait = new Retrait(rs.getString("rue") ,rs.getString("code_postal"),rs.getString("ville") );
 				result.add(retrait);
 			}
 		}catch (SQLException e) {
@@ -79,11 +60,9 @@ public class RetraitDAOImp {
 		}
 		return result;
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
 
 }
